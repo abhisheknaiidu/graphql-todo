98c0ad6172e771428283273744a30f234cbe905b
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-22 17:45:00 +0530

    Todo integrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 
+const GET_TODOS = gql`
+query getTodos {
+  todos {
+    done
+    id
+    text
+  }
+}
+`
+
+// List Todos
+// Add Todos
+// Toggle Todos
+// Delete Todos
 function App() {
+
+  const { loading, data } = useQuery(GET_TODOS)
+  // console.log(stuff)
+
+  if(loading) return <div>Loading...</div>
+
   return (
     <div>
-      App
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          <span>
+            {todo.text}
+          </span>
+          <button> &times; </button>
+        </p>
+      ))}
     </div>
   );
 }
