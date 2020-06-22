44f22c4a248366140606b88784233db70b8cce4a
abhisheknaiidu <geekyabhi12@gmail.com>
2020-06-22 17:28:18 +0530

    Setup appolo client 🌈

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri: 'https://graphql-tod.herokuapp.com/v1/graphql'
+})
+
+// We can direct pass client to the respective react component trees
+// Using it's in built feature
+
+// client.query({
+//   query: gql`
+//   query getTodos {
+//     todos {
+//       done
+//       id
+//       text
+//     }
+//   }
+//   `  
+// }).then( data => console.log(data))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
